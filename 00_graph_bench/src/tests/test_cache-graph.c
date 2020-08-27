8f9562eb66806f4303293fdae68e9146255da870
Abdullah Mughrabi <atmughra@ncsu.edu>
2020-08-27 11:06:45 -0400

    update list graphs

--- 00_graph_bench/src/tests/test_cache-graph.c
+++ 00_graph_bench/src/tests/test_cache-graph.c
@@ -49,7 +49,7 @@
 #endif
 
 #include "graphTest.h"
-#define GRAPH_NUM 4
+#define GRAPH_NUM 24
 #define MODE_NUM 3
 
 int numThreads;
@@ -69,49 +69,77 @@ main (int argc, char **argv)
     char label_dir[1024];
     char perf_file[1024];
 
+    // char *benchmarks_graphs[GRAPH_NUM] =
+    // {
+    //     "LAW-amazon-2008",
+    //     "LAW-cnr-2000",
+    //     "LAW-dblp-2010",
+    //     "LAW-enron"
+    // };
+
+    // char *benchmarks_dir[GRAPH_NUM] =
+    // {
+    //     "../01_test_graphs/LAW/LAW-amazon-2008",
+    //     "../01_test_graphs/LAW/LAW-cnr-2000",
+    //     "../01_test_graphs/LAW/LAW-dblp-2010",
+    //     "../01_test_graphs/LAW/LAW-enron"
+    // };
+
     char *benchmarks_graphs[GRAPH_NUM] =
     {
         "LAW-amazon-2008",
+        "LAW-arabic-2005",
         "LAW-cnr-2000",
         "LAW-dblp-2010",
-        "LAW-enron"
+        "LAW-enron",
+        "LAW-eu-2005",
+        "LAW-hollywood-2009",
+        "LAW-in-2004",
+        "LAW-indochina-2004",
+        "LAW-it-2004",
+        "LAW-ljournal-2008",
+        "LAW-sk-2005",
+        "LAW-uk-2002",
+        "LAW-uk-2005",
+        "LAW-webbase-2001",
+        "KONECT-wikipedia_link_en",
+        "GAP-road",
+        "GAP-twitter",
+        "GONG-gplus",
+        "SNAP-cit-Patents",
+        "SNAP-com-Orkut",
+        "SNAP-soc-LiveJournal1",
+        "SNAP-soc-Pokec",
+        "SNAP-web-Google"
     };
 
     char *benchmarks_dir[GRAPH_NUM] =
     {
-        "../01_test_graphs/LAW/LAW-amazon-2008",
-        "../01_test_graphs/LAW/LAW-cnr-2000",
-        "../01_test_graphs/LAW/LAW-dblp-2010",
-        "../01_test_graphs/LAW/LAW-enron"
+        "../../01_GraphDatasets/LAW/LAW-amazon-2008",
+        "../../01_GraphDatasets/LAW/LAW-arabic-2005",
+        "../../01_GraphDatasets/LAW/LAW-cnr-2000",
+        "../../01_GraphDatasets/LAW/LAW-dblp-2010",
+        "../../01_GraphDatasets/LAW/LAW-enron",
+        "../../01_GraphDatasets/LAW/LAW-eu-2005",
+        "../../01_GraphDatasets/LAW/LAW-hollywood-2009",
+        "../../01_GraphDatasets/LAW/LAW-in-2004",
+        "../../01_GraphDatasets/LAW/LAW-indochina-2004",
+        "../../01_GraphDatasets/LAW/LAW-it-2004",
+        "../../01_GraphDatasets/LAW/LAW-ljournal-2008",
+        "../../01_GraphDatasets/LAW/LAW-sk-2005",
+        "../../01_GraphDatasets/LAW/LAW-uk-2002",
+        "../../01_GraphDatasets/LAW/LAW-uk-2005",
+        "../../01_GraphDatasets/LAW/LAW-webbase-2001",
+        "../../01_GraphDatasets/KONECT/KONECT-wikipedia_link_en",
+        "../../01_GraphDatasets/GAP/GAP-road",
+        "../../01_GraphDatasets/GAP/GAP-twitter",
+        "../../01_GraphDatasets/GONG/GONG-gplus",
+        "../../01_GraphDatasets/SNAP/SNAP-cit-Patents",
+        "../../01_GraphDatasets/SNAP/SNAP-com-Orkut",
+        "../../01_GraphDatasets/SNAP/SNAP-soc-LiveJournal1",
+        "../../01_GraphDatasets/SNAP/SNAP-soc-Pokec",
+        "../../01_GraphDatasets/SNAP/SNAP-web-Google"
     };
-
-    // char *benchmarks_dir[GRAPH_NUM] =
-    // {
-    //     "../../01_GraphDatasets/LAW/LAW-amazon-2008",
-    //     "../../01_GraphDatasets/LAW/LAW-arabic-2005",
-    //     "../../01_GraphDatasets/LAW/LAW-cnr-2000",
-    //     "../../01_GraphDatasets/LAW/LAW-dblp-2010",
-    //     "../../01_GraphDatasets/LAW/LAW-enron",
-    //     "../../01_GraphDatasets/LAW/LAW-eu-2005",
-    //     "../../01_GraphDatasets/LAW/LAW-hollywood-2009",
-    //     "../../01_GraphDatasets/LAW/LAW-in-2004",
-    //     "../../01_GraphDatasets/LAW/LAW-indochina-2004",
-    //     "../../01_GraphDatasets/LAW/LAW-it-2004",
-    //     "../../01_GraphDatasets/LAW/LAW-ljournal-2008",
-    //     "../../01_GraphDatasets/LAW/LAW-sk-2005",
-    //     "../../01_GraphDatasets/LAW/LAW-uk-2002",
-    //     "../../01_GraphDatasets/LAW/LAW-uk-2005",
-    //     "../../01_GraphDatasets/LAW/LAW-webbase-2001",
-    //     "../../01_GraphDatasets/KONECT/KONECT-wikipedia_link_en",
-    //     "../../01_GraphDatasets/GAP/GAP-road",
-    //     "../../01_GraphDatasets/GAP/GAP-twitter",
-    //     "../../01_GraphDatasets/GONG/GONG-gplus",
-    //     "../../01_GraphDatasets/SNAP/SNAP-cit-Patents",
-    //     "../../01_GraphDatasets/SNAP/SNAP-com-Orkut",
-    //     "../../01_GraphDatasets/SNAP/SNAP-soc-LiveJournal1",
-    //     "../../01_GraphDatasets/SNAP/SNAP-soc-Pokec",
-    //     "../../01_GraphDatasets/SNAP/SNAP-web-Google"
-    // };
 
     char *reorder_labels[MODE_NUM] =
     {
