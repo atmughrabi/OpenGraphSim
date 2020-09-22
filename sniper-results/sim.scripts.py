df9fb4f0ac9a82bdc058b8fb8c2d0df6972e9930
Abdullah Mughrabi <atmughra@ncsu.edu>
2020-09-22 16:21:59 -0400

    [DIR] removed sniper results folder

--- sniper-results/sim.scripts.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.argv = [ "/home/ab/Documents/github_repos/05_OpenGraphSim/00_graph_bench/sniper/scripts/markers.py", "verbose:markers:stats" ]
-execfile("/home/ab/Documents/github_repos/05_OpenGraphSim/00_graph_bench/sniper/scripts/markers.py")
